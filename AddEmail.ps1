e3706e363a573dc0882cb316375ce7d773dc081c
dkim237 <114041496+dkim237@users.noreply.github.com>
2022-09-20 19:35:31 -0700

    Update AddEmail.ps1

--- AddEmail.ps1
+++ AddEmail.ps1
@@ -1,4 +1,6 @@
-﻿$users = import-csv -path C:\userlist.csv
+# defines user list based on CSV file
+$users = import-csv -path C:\userlist.csv
+# iterates through each user in CSV, locates matching user based on samAccountName, pipes this to command to define email address field in AD
 foreach ($user in $users){
-    Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" | Set-ADUser -EmailAddress $($user.samaccountname + '@ivantisinc.com')
+    Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" | Set-ADUser -EmailAddress $($user.samaccountname + '@domain.com')
     }
