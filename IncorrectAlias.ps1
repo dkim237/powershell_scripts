79b2f6743423396431f154189dbb370fe83aee64
dkim237 <114041496+dkim237@users.noreply.github.com>
2022-09-20 20:07:17 -0700

    Update IncorrectAlias.ps1

--- IncorrectAlias.ps1
+++ IncorrectAlias.ps1
@@ -1,9 +1,11 @@
-﻿$users = Import-CSV -path "C:\Powershell\UserAlias\Test.csv"
+#
+
+$users = Import-CSV -path "C:\Powershell\UserAlias\Test.csv"
 $CompleteReport=@()
 foreach ($user in $users) {
     $firstname = $user.first
     $lastname = $user.last
-    $fullusername =  ("{0}.{1}@antisroofing.com" -f $firstname,$lastname)
+    $fullusername =  ("{0}.{1}@domain.com" -f $firstname,$lastname)
     $aduser = Get-ADUser -filter * | where UserPrincipalName -notlike "$fullusername"
     $CompleteReport = $CompleteReport+$aduser
 }
