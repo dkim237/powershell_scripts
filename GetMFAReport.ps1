20bb90da30fb5e50c9a3a166d8d7eae343a31aaf
dkim237 <114041496+dkim237@users.noreply.github.com>
2022-09-20 19:56:08 -0700

    Update GetMFAReport.ps1

--- GetMFAReport.ps1
+++ GetMFAReport.ps1
@@ -1,36 +1,38 @@
-Write-Host "Finding Azure Active Directory Accounts..."
-$Users = Get-MsolUser -All | ? { $_.UserType -ne "Guest" }
-$Report = [System.Collections.Generic.List[Object]]::new() # Create output file
-Write-Host "Processing" $Users.Count "accounts..." 
-ForEach ($User in $Users) {
-    $MFAMethods = $User.StrongAuthenticationMethods.MethodType
-    $MFAEnforced = $User.StrongAuthenticationRequirements.State
-    $MFAPhone = $User.StrongAuthenticationUserDetails.PhoneNumber
-    $DefaultMFAMethod = ($User.StrongAuthenticationMethods | ? { $_.IsDefault -eq "True" }).MethodType
-    If (($MFAEnforced -eq "Enforced") -or ($MFAEnforced -eq "Enabled")) {
-        Switch ($DefaultMFAMethod) {
-            "OneWaySMS" { $MethodUsed = "One-way SMS" }
-            "TwoWayVoiceMobile" { $MethodUsed = "Phone call verification" }
-            "PhoneAppOTP" { $MethodUsed = "Hardware token or authenticator app" }
-            "PhoneAppNotification" { $MethodUsed = "Authenticator app" }
+# Script to generate an organized report on the status of MFA enforcement across an entire O365/AAD tenant
+
+Write-Host "Finding Azure Active Directory Accounts..." # simple message indicating current action
+$Users = Get-MsolUser -All | ? { $_.UserType -ne "Guest" } # stores all AAD users excluding any guest accounts in object Users
+$Report = [System.Collections.Generic.List[Object]]::new() # Creates formatted table output file
+Write-Host "Processing" $Users.Count "accounts..." # notifies user of total number of user objects discovered stored in Users variable
+ForEach ($User in $Users) { # iterating through each user object 
+    $MFAMethods = $User.StrongAuthenticationMethods.MethodType # stores the type of MFA method utilized, defined below
+    $MFAEnforced = $User.StrongAuthenticationRequirements.State # stores whether MFA is enforced/enabled for the user
+    $MFAPhone = $User.StrongAuthenticationUserDetails.PhoneNumber # stores if user has MFA phone number registered
+    $DefaultMFAMethod = ($User.StrongAuthenticationMethods | ? { $_.IsDefault -eq "True" }).MethodType # stores MFA method if current is default option
+    If (($MFAEnforced -eq "Enforced") -or ($MFAEnforced -eq "Enabled")) { # checks if value stored in MFAMethods is either enforced or enabled
+        Switch ($DefaultMFAMethod) { # switch statement to evaluate series of parameters for which mfa method is utilized
+            "OneWaySMS" { $MethodUsed = "One-way SMS" } # if SMS is used, stored as defined
+            "TwoWayVoiceMobile" { $MethodUsed = "Phone call verification" } # if phone call option is used, stored as defined
+            "PhoneAppOTP" { $MethodUsed = "Hardware token or authenticator app" } # OTP used, stored as defined
+            "PhoneAppNotification" { $MethodUsed = "Authenticator app" } # OTP + push notification, stored as defined
         }
     }
-    Else {
+    Else { # if MFAEnforced is neither enabled or enforced, stored as not enabled and not used
         $MFAEnforced = "Not Enabled"
         $MethodUsed = "MFA Not Used" 
     }
   
-    $ReportLine = [PSCustomObject] @{
-        User        = $User.UserPrincipalName
-        Name        = $User.DisplayName
-        MFAUsed     = $MFAEnforced
-        MFAMethod   = $MethodUsed 
-        PhoneNumber = $MFAPhone
+    $ReportLine = [PSCustomObject] @{ # stores below variables in a single line to be added to final report
+        User        = $User.UserPrincipalName # user displayed as UPN (email format)
+        Name        = $User.DisplayName # Display Name (full name of user)
+        MFAUsed     = $MFAEnforced # whether MFA is enforced, enabled, or not enabled
+        MFAMethod   = $MethodUsed # type of MFA used which is either sms, call, otp, or push notif
+        PhoneNumber = $MFAPhone # phone number used for MFA
     }
                  
-    $Report.Add($ReportLine) 
+    $Report.Add($ReportLine) # adds each line generated into one larger table
 }
 
-Write-Host "Report is in c:\temp\MFAUsers.CSV"
-$Report | Select Name, MFAUsed, MFAMethod, PhoneNumber | Sort Name | Out-GridView
-$Report | Sort Name | Export-CSV -NoTypeInformation c:\temp\MFAUsers.csv
+Write-Host "Report is in c:\temp\MFAUsers.CSV" # notifies user where report is being saved in csv format
+$Report | Select Name, MFAUsed, MFAMethod, PhoneNumber | Sort Name | Out-GridView # selects all items and outputs in table display with column headers
+$Report | Sort Name | Export-CSV -NoTypeInformation c:\temp\MFAUsers.csv # generates and saves report to location here
