a409a72359063af19f42867751b50661dd0cce59
dkim237 <114041496+dkim237@users.noreply.github.com>
2022-09-20 19:37:16 -0700

    Update CopyADGroupsTest.ps1

--- CopyADGroupsTest.ps1
+++ CopyADGroupsTest.ps1
@@ -1,3 +1,7 @@
-﻿$currentuser = Read-Host -Prompt 'Input existing user to copy from:'
+# Script to quickly copy AD group memberships from one user to another
+
+# Prompts user for original we are copying from, and user we are copying to
+$currentuser = Read-Host -Prompt 'Input existing user to copy from:'
 $newuser = Read-Host -Prompt 'Input new user to copy to:'
+# Locates user based on filtering, selects memberof attribute, then applies this to our target user
 Get-AdUser -Filter "name -like '*$currentuser*'" -Properties memberof | Select-Object -ExpandProperty memberof | Add-ADGroupMember -Members -Filter "name -like '*$newuser*'"
